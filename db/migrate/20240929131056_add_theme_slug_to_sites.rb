cc805b505aa2d45f02063c8473fc529a37b74b31
Gleydson Tavares <tavares.gleydson@gmail.com>
2024-09-29 10:33:34 -0300

    adds theme_id to sites

---
+++ db/migrate/20240929131056_add_theme_slug_to_sites.rb
@@ -0,0 +1,6 @@
+class AddThemeSlugToSites < ActiveRecord::Migration[7.2]
+  def change
+    add_column :maglev_sites, :theme_id, :string, default: "default", null: false
+    add_index :maglev_sites, :theme_id
+  end
+end
