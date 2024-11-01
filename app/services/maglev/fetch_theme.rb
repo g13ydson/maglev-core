0037d4ea625e385082d80397e8f24d0f3ccf934e
Gleydson Tavares <tavares.gleydson@gmail.com>
2024-11-01 10:08:46 -0300

    fetch theme based on site theme_id

--- app/services/maglev/fetch_theme.rb
+++ app/services/maglev/fetch_theme.rb
@@ -5,8 +5,16 @@ module Maglev
   class FetchTheme
     include Injectable
 
+    dependency :context
+
     def call
-      Maglev.local_themes.first
+      Maglev.local_themes.detect {|theme| theme.id == site.theme_id }
+    end
+
+    protected
+
+    def site
+      @site ||= context.site
     end
   end
 end
